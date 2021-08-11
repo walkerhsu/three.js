9dd3be443448436592a72bed96fb7214a4e46868
walkerhsu <yp10731155@gmail.com>
2021-08-11 18:44:01 +0800

    solor system

---
+++ src/main.js
@@ -0,0 +1,147 @@
+function bodyLoaded() {
+    main()
+}
+
+function main() {
+    const canvas = document.querySelector('#Canvas');
+    const renderer = new THREE.WebGLRenderer({canvas});
+    const gui = new dat.GUI();
+
+    const fov = 40;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 50, 0);
+    camera.up.set(0, 0, 1);
+    camera.lookAt(0, 0, 0);
+
+    const scene = new THREE.Scene();
+
+    // an array of objects whose rotation to update
+    const objects = [];
+    
+    // use just one sphere for everything
+    const radius = 1;
+    const widthSegments = 6;
+    const heightSegments = 6;
+    const sphereGeometry = new THREE.SphereGeometry(
+        radius, widthSegments, heightSegments);
+    
+    const solarSystem = new THREE.Object3D();
+    scene.add(solarSystem);
+    objects.push(solarSystem);
+        
+    const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
+    const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+    sunMesh.scale.set(5, 5, 5);
+    solarSystem.add(sunMesh);
+    objects.push(sunMesh);
+        
+    const earthOrbit = new THREE.Object3D();
+    earthOrbit.position.x = 10;
+    solarSystem.add(earthOrbit);
+    objects.push(earthOrbit);
+
+    const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
+    const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+    earthOrbit.add(earthMesh);
+    objects.push(earthMesh);
+
+    const moonOrbit = new THREE.Object3D();
+    moonOrbit.position.x = 2;
+    earthOrbit.add(moonOrbit);
+    
+    const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
+    const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+    moonMesh.scale.set(.5, .5, .5);
+    moonOrbit.add(moonMesh);
+    objects.push(moonMesh);
+
+    class AxisGridHelper {
+        constructor(node, units = 10) {
+            const axes = new THREE.AxesHelper();
+            axes.material.depthTest = false;
+            axes.renderOrder = 2;  // after the grid
+            node.add(axes);
+        
+            const grid = new THREE.GridHelper(units, units);
+            grid.material.depthTest = false;
+            grid.renderOrder = 1;
+            node.add(grid);
+        
+            this.grid = grid;
+            this.axes = axes;
+            this.visible = false;
+        }
+        get visible() {
+            return this._visible;
+        }
+        set visible(v) {
+            this._visible = v;
+            this.grid.visible = v;
+            this.axes.visible = v;
+        }
+    }
+    
+    function makeAxisGrid(node, label, units) {
+        const helper = new AxisGridHelper(node, units);
+        gui.add(helper, 'visible').name(label);
+    }
+    
+    makeAxisGrid(solarSystem, 'solarSystem', 26);
+    makeAxisGrid(sunMesh, 'sunMesh');
+    makeAxisGrid(earthOrbit, 'earthOrbit');
+    makeAxisGrid(earthMesh, 'earthMesh');
+    makeAxisGrid(moonOrbit, 'moonOrbit');
+    makeAxisGrid(moonMesh, 'moonMesh');
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 3;
+        const light = new THREE.PointLight(color, intensity);
+        scene.add(light);
+    }
+
+    function render(time) {
+        time *= 0.001;  // convert time to seconds
+       
+        objects.forEach((obj) => {
+            obj.rotation.y = time;
+        });
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        renderer.render(scene, camera);
+       
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+}
+
+function makeInstance(scene , geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+}
+
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const pixelRatio = window.devicePixelRatio;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
